19eda0f6b0787d2592fc8e3acc31d1be18082f04
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2011-07-29 09:47:53 +0900

    ユニットテスト追加

---
+++ test/Array/include.test.js
@@ -0,0 +1,47 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/*!
+ * Next Core
+ * Copyright(c) 2006-2011 Xenophy.CO.,LTD All rights Reserved.
+ * http://www.xenophy.com
+ */
+
+// {{{ requires
+
+require('NX.Core');
+require('should');
+assert = require('assert');
+
+// }}}
+// {{{ NX.Array.include Tests
+
+module.exports = {
+
+    // {{{ 'test include#pattern1'
+
+    'test include#pattern1': function() {
+
+        var arr;
+
+        arr = [1,2,3,4,5];
+
+        NX.Array.include(arr, 3);
+        arr.length.should.equal(5);
+
+        NX.Array.include(arr, 6);
+        arr.length.should.equal(6);
+    }
+
+    // }}}
+
+};
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
---
+++ test/util/Format/nl2br.test.js
@@ -0,0 +1,48 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/*!
+ * Next Core
+ * Copyright(c) 2006-2011 Xenophy.CO.,LTD All rights Reserved.
+ * http://www.xenophy.com
+ */
+
+// {{{ requires
+
+require('NX.Core');
+require('should');
+assert = require('assert');
+
+// }}}
+// {{{ NX.util.Format.nl2br Tests
+
+module.exports = {
+
+    // {{{ 'test nl2br#pattern1'
+
+    'test nl2br#pattern1': function(beforeExit) {
+
+        var tpl, ret;
+
+        tpl = [
+            'line1',
+            'line2'
+        ].join("\n");
+
+        ret = NX.util.Format.nl2br(tpl);
+
+        ret.should.equal('line1<br />line2');
+    }
+
+    // }}}
+
+};
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
